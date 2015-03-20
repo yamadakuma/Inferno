be61f833be4c807ad228a7c03330bda1bc7ebef0
yamadakuma <bajadragon.321@gmail.com>
2015-03-20 12:17:08 -0400

    Get through one night without you?

--- Inferno/src/inferno/enemies/Archer.java
+++ Inferno/src/inferno/enemies/Archer.java
@@ -1,6 +1,22 @@
 package inferno.enemies;
 
+import inferno.Player;
+
 public class Archer extends Enemy {
+	
+	@Override
+	public void checkPlayerSpotted(){
+		int x=getPosX(), y=getPosY();
+		Player p=getWorld().getPlayer();
+		if(Math.abs(p.getPosX()-x)<4||Math.abs(p.getPosY()-y)<4){ //this is a temporary thing all enemies have blindivision for now a better vision alogortihm will be written when time is avalible
+			setPlayerSpotted(true);
+			
+			
+			
+		}
+		else setPlayerSpotted(false);
+	}
+	
 	@Override
 	public void step() {  //algorithm that selects movement and attacks
 		if(isPlayerSpotted()){
